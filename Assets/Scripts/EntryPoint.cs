3b9eb1b07cfa60988c3fd8fb64c00c97c70becd7
ChapaCactus <moboratory@gmail.com>
2023-10-08 15:07:53 +0900

    起動クラス

---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace MOB.Framework.Game2D
+{
+    /// <summary>
+    /// 起動時のエントリーポイントクラス.
+    /// </summary>
+    public static class EntryPoint
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void GameBoot()
+        {
+            // TODO: ゲーム起動.
+        }
+    }
+}
