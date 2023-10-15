c5441fad3922cf4f2dc348acc894ce09b4e909b6
ChapaCactus <moboratory@gmail.com>
2023-10-15 19:29:15 +0900

    レイアウト設定後のフォーマット

--- Assets/Scripts/Samples/SampleDialog.cs
+++ Assets/Scripts/Samples/SampleDialog.cs
@@ -6,6 +6,14 @@ namespace MOB.Framework.Game2D.Samples
 {
     public sealed class SampleDialog : DialogBase<SampleDialog.ViewData>, IDialog<SampleDialog.ViewData>
     {
+        public struct ViewData
+        {
+            public string Title { get; set; }
+            public string Body { get; set; }
+
+            public IReadOnlyList<ButtonData> FooterButtonDataList { get; set; }
+        }
+
         public override void Initialize(ViewData viewData)
         {
             AddButtons(viewData.FooterButtonDataList);
@@ -29,14 +37,6 @@ namespace MOB.Framework.Game2D.Samples
         private void AddButtons(IReadOnlyList<ButtonData> buttonDataList)
         {
             foreach (ButtonData buttonData in buttonDataList) AddButton(buttonData);
-        }
-
-        public struct ViewData
-        {
-            public string Title { get; set; }
-            public string Body { get; set; }
-
-            public IReadOnlyList<ButtonData> FooterButtonDataList { get; set; }
         }
     }
 }
